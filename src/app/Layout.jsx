87f962a7492d5aeecf979d36ac37e1628b4c0347
David Luong <hoai0906@gmail.com>
2024-02-27 21:34:05 +0800

    add home

--- src/app/Layout.jsx
+++ src/app/Layout.jsx
@@ -6,6 +6,9 @@ const Layout = () => {
       <nav className="menu">
         <ul>
           <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
             <Link to="/compare">Compare</Link>
           </li>
           <li>
