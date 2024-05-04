4b60d7e9bca816713b712263de7a7064b49f84cf
David Luong <hoai0906@gmail.com>
2024-05-04 14:55:00 +0800

    icon

--- src/app/bep20-list/Bep20List.jsx
+++ src/app/bep20-list/Bep20List.jsx
@@ -92,8 +92,8 @@ const Bep20List = () => {
 
   const colDefs = [
     {
-      field: "Actions",
-      width: 80,
+      field: "...",
+      width: 60,
       valueGetter: (params) => params.data,
       cellRenderer: (params) => {
         const data = params.data;
@@ -112,7 +112,13 @@ const Bep20List = () => {
         }
         let id = `${data.currency}_${data.contractAddress}_kucoin_${bnbPool}_${amount}`;
 
-        return <button onClick={() => navigate("/token-detail/" + id)}> Add </button>;
+        return (
+          <span onClick={() => navigate("/token-detail/" + id)}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="22px">
+              <path d="M64 80c-8.8 0-16 7.2-16 16V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V96c0-8.8-7.2-16-16-16H64zM0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM200 344V280H136c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H248v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z" />
+            </svg>
+          </span>
+        );
       },
     },
     { field: "pair", width: 110 },
